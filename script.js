3a04971db98dd88a8532fdd714e6c229697b7b95
Drew Bendler <drewbendler5@gmail.com>
2020-02-07 09:03:06 -0500

    first api connection

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // liquid curve twice please spot near hub stem birth foster birth badge
+let menu = document.getElementById('nav');
+
+fetch('https://rest.coinapi.io/v1/exchanges', {
+    headers: {
+        'x-api-key': 'DF1E61FA-326F-4FFA-BF19-5B31139A6463'
+    }
+});
+
+menu.addEventListener('click', menuDropDown);
+
+let baseUrl = 'https://rest.coinapi.io/v1/exchanges';
+
+function menuDropDown (e) {
+	e.preventDefault();
+
+fetch(baseUrl).then(res => {
+return res.json();
+})
+.then(res => {
+	let exchange = '/name/';
+	console.log(baseUrl + exchange, res);
+})
+.catch(err => {
+	console.log('failed...')
+})
+}
+
+
+
+
+
+
+
