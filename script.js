d0b933eee99f90433a772f5ea2b7bdc6d28b1e17
Drew Bendler <drewbendler5@gmail.com>
2020-02-07 14:14:11 -0500

    final css/ link-api connection

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 // liquid curve twice please spot near hub stem birth foster birth badge
-let menu = document.getElementById('nav');
+let bitcoinLink = document.getElementById('link1');
+let ethereumLink = document.getElementById('link2');
+let litecoinLink = document.getElementById('link3');
+let rippleLink = document.getElementById('link4');
+let dogecoinLink = document.getElementById('link5');
+
+let baseUrl = "https://rest.coinapi.io/v1/exchanges"; 
+
+let iconDiv = document.getElementById("iconpic");
+
+let descripton = document.getElementById('object-description');
+
+bitcoinLink.addEventListener('click', testFunc);
+
+function testFunc (e) {
+	e.preventDefault();
+	console.log('test successful');
+	
+}
+
+
 
 fetch('https://rest.coinapi.io/v1/exchanges', {
     headers: {
         'x-api-key': 'DF1E61FA-326F-4FFA-BF19-5B31139A6463'
     }
 });
-
-menu.addEventListener('click', menuDropDown);
-
-let baseUrl = 'https://rest.coinapi.io/v1/exchanges';
-
-function menuDropDown (e) {
-	e.preventDefault();
-
-fetch(baseUrl).then(res => {
-return res.json();
-})
-.then(res => {
-	let exchange = '/name/';
-	console.log(baseUrl + exchange, res);
-})
-.catch(err => {
-	console.log('failed...')
-})
-}
-
 
 
 
